4db0c878d4563d5194a734e248e21ca0d2416f3e
NasirUddinNahid <nahidahmed17762@gmail.com>
2022-03-01 01:28:49 +0600

    done

--- the-dog-api/the-card-Api/card.js
+++ the-dog-api/the-card-Api/card.js
@@ -13,40 +13,62 @@ const searchButton = () => {
         // alert("not a number")
         errorMsg.innerText = "Please give a number"
         input.value = "";
-        main.innerHTML="";
+        main.innerHTML = "";
     }
     else if (inputValue < 0) {
         errorMsg.innerText = "please give a positive number"
         input.value = "";
-        main.innerHTML="";
-      
+        main.innerHTML = "";
+
     }
 
     else {
-        main.innerHTML="";
+        main.innerHTML = "";
         fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=${inputValue}`)
             .then(res => res.json())
             .then(data => Cardsdisplay(data.cards))
-            input.value = "";
-            errorMsg.innerText="";
+        input.value = "";
+        errorMsg.innerText = "";
     }
 }
 const Cardsdisplay = (cards) => {
-    
-    for(const card of cards){
-        const div=document.createElement('div')
-        div.classList.add("col-lg-4")       
+
+    for (const card of cards) {
+        const div = document.createElement('div')
+        div.classList.add("col-lg-4")
         div.classList.add("mb-5")
-        div.innerHTML=`
+        div.innerHTML = `
         <div class="card" style="width: 18rem;">
   <img src="${card.image}" class="card-img-top" alt="...">
   <div class="card-body">
     <h5 class="card-title">${card.suit}</h5>
     <p class="card-text">${card.code}</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
+    <button onclick="cardDetails('${card.code}')" class="btn btn-primary">See Details</button>
   </div>
 </div>
         `
         main.appendChild(div)
     }
 }
+
+const cardDetails = (code) => {
+    fetch(`https://deckofcardsapi.com/api/deck/new/draw/?count=52`)
+        .then(res => res.json())
+        .then(data => {
+            const allCards = data.cards;
+            const singleCard = allCards.find(card => card.code === code)
+            const div = document.createElement("div");
+            main.innerHTML = "";
+            div.innerHTML = `
+                <div class="card" style="width: 18rem;">
+                    <img src="${singleCard.image}" class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${singleCard.suit}</h5>
+                        <p class="card-text">${singleCard.code}</p>
+                        <p class="card-text">${singleCard.value}</p>
+                    </div>
+                </div>
+            `
+            main.appendChild(div)
+        })
+    }
